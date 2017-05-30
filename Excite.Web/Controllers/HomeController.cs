6a6febbbf8c2ff229c91fdfdbecbf456ea7d3c26
sushant2517 <skulkarni.net@gmail.com>
2017-05-30 14:44:54 +1000

    -- adding a web interface

---
+++ Excite.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Excite.Utilities.Extensions;
+using Excite.Web.Models;
+
+namespace Excite.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(IndexViewModel model)
+        {
+            if(!ModelState.IsValid)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var result = model.Text.IndicesOf(model.SubText);
+            model.Result = string.Join(",", result);
+
+            return View(model);
+        }
+    }
+}
---
+++ Excite.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Excite.Web.MvcApplication" Language="C#" %>
